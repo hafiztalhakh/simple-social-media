11e2542681700c8ff5b2637b82db39507bd10c02
Talha Khalid <hafiz.talhakh@gmail.com>
2025-04-17 14:38:00 +0500

    Static implementation (#6)
    * Routing issues fixed
    
    * checking
    
    * Token issue fixed
    
    * Auth condition remove

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -19,10 +18,7 @@ function App() {
         <CssBaseline />
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route
-            path="/home"
-            element={isAuth ? <HomePage /> : <Navigate to="/" />}
-          />
+          <Route path="/home" element={<HomePage />} />
           <Route
             path="/profile/:userId"
             element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
